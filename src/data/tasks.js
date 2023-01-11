af574c1b863a5bcebdd933b413b9357d23e11a37
Yheffer <yhou200@gmail.com>
2023-01-11 04:31:13 -0500

    first react app

---
+++ src/data/tasks.js
@@ -0,0 +1,19 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Tale",
+        description: "Narrative"
+    },
+
+    {
+        id: 1,
+        title: "Report",
+        description: "Summary"
+    },
+
+    {
+        id: 2,
+        title: "Essay",
+        description: "Analytic"
+    }
+];
